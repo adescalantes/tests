f6284d09ee8b968d8c7d34270a30edbfe3dd40f7
aescalan <aescalan@LIM-5CD5447V14.usersad.everis.int>
2019-05-14 16:39:07 -0500

    test10

--- test/src/main/java/com/trainee/controllers/FamilyMemberController.java
+++ test/src/main/java/com/trainee/controllers/FamilyMemberController.java
@@ -1,8 +1,26 @@
 package com.trainee.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.trainee.models.FamilyMember;
+import com.trainee.services.FamilyMemberService;
+
 @RestController
+@RequestMapping("/v1")
 public class FamilyMemberController {
 
+  @Autowired
+  @Qualifier("familyMemberServ")
+  private FamilyMemberService familyMember;
+
+  @GetMapping("/familyMembers")
+  public List<FamilyMember> getFamilies() {
+    return familyMember.getAllFamilyMember();
+  }
 }
--- test/src/main/java/com/trainee/services/FamilyMemberService.java
+++ test/src/main/java/com/trainee/services/FamilyMemberService.java
@@ -1,8 +1,22 @@
 package com.trainee.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import com.trainee.models.FamilyMember;
+import com.trainee.repositories.FamilyMemberRepository;
 
 @Service(value = "familyMemberServ")
 public class FamilyMemberService {
 
+  @Autowired
+  @Qualifier("familyMemberRepo")
+  private FamilyMemberRepository familyMember;
+
+  public List<FamilyMember> getAllFamilyMember() {
+    return familyMember.findAll();
+  }
 }
